99e3c8783af900b081567e60b67c11ce51a11930
johann <vyavya2003@yandex.ru>
2023-03-22 20:41:04 +0300

    add sample project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import jade.core.Agent;
-
-public class Main extends Agent {
-    protected void setup() {
-        System.out.println("Hello World. My name is " + getLocalName());
+public class Main {
+    public static void main(String[] args) {
+        MainController mainController = new MainController();
+        mainController.initAgents("org.example.agents");
     }
 }
