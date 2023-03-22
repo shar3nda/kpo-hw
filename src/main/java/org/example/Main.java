d9837d228990796c5c44e89996767fa905553586
johann <vyavya2003@yandex.ru>
2023-03-22 19:45:54 +0300

    add jade dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import jade.core.Agent;
+
+public class Main extends Agent {
+    protected void setup() {
+        System.out.println("Hello World. My name is " + getLocalName());
     }
 }
